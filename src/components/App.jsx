abd4e6fe5022686f91a8e1cefdef889ba848f2c0
GIT13Olena <girnyak.1olena@gmail.com>
2024-01-24 14:36:57 +0200

    Created a modal window with minimal styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,48 @@
+import React, { useState } from 'react';
+import close from '../icons/close-circle.svg'
+
 function App() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleOnClose = e => {
+    if (e.code === 'Escape') {
+      closeModal();
+    }
+  };
+
+  window.addEventListener('keydown', handleOnClose);
+
+  const handleOverlyClick = e => {
+    if (e.currentTarget === e.target) {
+      closeModal();
+    }
+  };
+
    return (
      <div className="App">
-       Шаблоний pепозиторій для твого React проекту
+
+      {(isModalOpen) && (
+        <div className="modalOverly" onClick={handleOverlyClick}></div>
+      )}
+
+       <button onClick={openModal} className="open-button">Open modal</button>
+
+       {isModalOpen && (
+           <>
+            <div className="modalUserContent">
+              <img src={close} alt="close svg" onClick={closeModal} className="svg-close"/>
+            </div>
+           </>
+          )}
+
      </div>
    );
  }
